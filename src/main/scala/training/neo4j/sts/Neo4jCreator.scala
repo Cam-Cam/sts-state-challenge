e5231a04f1dc1731b4f428ffe3074ba74ea405ed
Camille <camille.dufau@gmail.com>
2018-05-04 15:17:50 +0100

    Create a Neo4j creator that open a session and get the result of a query

--- src/main/scala/training/neo4j/sts/Neo4jCreator.scala
+++ src/main/scala/training/neo4j/sts/Neo4jCreator.scala
@@ -1,25 +1,30 @@
 package training.neo4j.sts
 
+import com.sun.xml.internal.bind.v2.TODO
 import org.neo4j.driver.v1._
+
+import scala.collection.JavaConverters._
 
 case class Neo4jCreator(
                          uri: String,
                         user: String,
                         password: String) extends GraphDatabase{
 
-  def writeQuery(query:String) = {
+  def runCypherQuery(query:String) : List[Record] = {
 
-    val driver = GraphDatabase.driver(uri,AuthTokens.basic(user,password))
+    //TODO Handle the case where the query is null
+    val driver: Driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
+    val transaction: Transaction = driver.session().beginTransaction()
 
-    val transaction = driver.session().beginTransaction()
-
-    transaction.run(query)
-
+    transaction.run(query).list().asScala.toList
   }
 
+  def runCypherQueries(queries: List[String]) : List[List[Record]] = {
 
+    //TODO Create a type for queries
+    queries.map(q=>runCypherQuery(q))
 
-
+  }
 
 
 
--- src/test/scala/training/neo4j/sts/Neo4jCreatorTest.scala
+++ src/test/scala/training/neo4j/sts/Neo4jCreatorTest.scala
@@ -1,11 +1,61 @@
 package training.neo4j.sts
 
+import org.neo4j.driver.v1.{AuthTokens, GraphDatabase}
 import org.scalatest.FunSuite
+import scala.collection.JavaConverters._
+
 
 
 class Neo4jCreatorTest extends FunSuite {
-  test("create a simple match query") {
 
+//  test("write an empty query") {
+//
+//    val uri: String = "bolt://localhost:7687"
+//    val user: String = "neo4j"
+//    val password:String = "231287"
+//
+//    val neo4jCreator: Neo4jCreator = Neo4jCreator(uri,user,password)
+//
+//    val expected: Boolean = true
+//    val actual: Boolean = neo4jCreator.runCypherQuery("").list().isEmpty
+//    assert(actual == expected)
+//
+//  }
+
+
+  test("send a simple match query") {
+
+    val uri: String = "bolt://localhost:7687"
+    val user: String = "neo4j"
+    val password:String = "231287"
+
+    val neo4jCreator: Neo4jCreator = Neo4jCreator(uri,user,password)
+
+    val expected: Int = 2
+    val actual: Int = neo4jCreator.runCypherQuery("MATCH (n) RETURN n").length
+    assert(actual == expected)
+
+    println(neo4jCreator.runCypherQuery("MATCH (n) RETURN n"))
 
   }
+
+  test("read a query file") {
+
+    val uri: String = "bolt://localhost:7687"
+    val user: String = "neo4j"
+    val password:String = "231287"
+    val file="queries.txt"
+
+    //Create neo4j session
+    val neo4jCreator: Neo4jCreator = Neo4jCreator(uri,user,password)
+    val queries = CypherQueries().createListOfQueries(file)
+
+   val expected: Int = 2
+
+   val actual: Int = neo4jCreator.runCypherQueries(queries).length //TODO
+
+    assert(actual == expected)
+
+  }
+
 }
