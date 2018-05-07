bf4554160a1d81ef5188a924d09b000dba73067e
Camille <camille.dufau@gmail.com>
2018-05-07 13:00:29 +0100

    Final work on parser

--- src/test/scala/training/neo4j/sts/JsonParserTest.scala
+++ src/test/scala/training/neo4j/sts/JsonParserTest.scala
@@ -1,5 +1,22 @@
 package training.neo4j.sts
 
-class JsonParserTest {
+import org.scalatest.FunSuite
+
+import scala.io.Source
+
+class JsonParserTest extends FunSuite {
+
+  test("parse initial file") {
+println(JsonParser.readFile("src/test/ressources/initial.json"))
+  }
+
+  test("extract component") {
+    val jvalue = JsonParser.readFile("src/test/ressources/initial.json")
+
+    println(JsonParser.createComponent(jvalue))
+
+
+  }
+
 
 }
